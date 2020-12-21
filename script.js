1486aa660ac32e92e812a73d2e1103a6087fae12
Caitlin Greer <clento1121@gmail.com>
2020-12-21 19:43:46 +0000

    fixed error with only searching for one state, and set maxLimit=10

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ const apiKey = 'ZdeDHGIZLomANuO9KnLl9fRXr51h2RpwOpo5Q057';
 const searchUrl = 'https://developer.nps.gov/api/v1/parks';
 
 function formatQueryParams(params){
-  const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+
+  return queryItems.join("&");
 }
 
 function displayResults(responseJson){
-  console.log(responseJson);
+ // console.log(responseJson);
 //clear previous results
   $('.js-results-list').empty();
 
 //iterate through results array
 for (let i = 0; i < responseJson.data.length; i++){
   $('.js-results-list').append(
-    `<li><h3><a href ="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+    `<li>
+      <h3><a href ="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+      <p class="address-info">${responseJson.data[i].addresses[0].line1}</p>
+      <p class="address-info">${responseJson.data[i].addresses[0].line2}</p>
+      <p>
+      ${responseJson.data[i].addresses[0].city}, 
+      ${responseJson.data[i].addresses[0].stateCode}
+      ${responseJson.data[i].addresses[0].postalCode}
+      </p>
     <p>${responseJson.data[i].description}</p>
     </li>`
   )};
@@ -27,10 +37,10 @@ $('#results').removeClass('hidden');
 
 }
 
-function getStateParks(searchTerm, maxResults) {
+function getStateParks(stateInput, maxResults=10) {
   const params = {
     key: apiKey,
-    stateCode: searchTerm,
+    q: stateInput,
     limit: maxResults
   };
   const queryString = formatQueryParams(params);
@@ -50,16 +60,13 @@ fetch(url)
   });
 }
 
-
-
-
 function watchForm() {
   console.log('watchForm is working')
   $('.js-form').submit(event => {
     event.preventDefault();
-    const searchTerm = $('.state-input').val();
+    const stateInput = $('.state-input').val();
     const maxResults = $('.max-results').val();
-    getStateParks(searchTerm, maxResults);
+    getStateParks(stateInput, maxResults);
   })
 }
 
